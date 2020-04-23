2a1118cbd5cbffca5029fe0e6ed6d44c935e9714
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-04-23 17:46:21 -0400

    Finished task 3

--- index.js
+++ index.js
@@ -140,6 +140,20 @@ class Car {
 */
 class Lambdasian {
 
+    constructor(attributes) {
+
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+
+    }
+
+    speak() {
+
+      return "Hello my name is " + this.name + ", " + "I am from " + this.location;
+
+    }
+
 }
 
 /*
