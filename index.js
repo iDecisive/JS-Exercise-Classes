cfbf3951bfa8cf5651b8884418cc0a9c02ead3b5
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-04-23 15:40:13 -0400

    Finished task 1

--- index.js
+++ index.js
@@ -41,6 +41,37 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+
+  }
+
+  eat(someFood) {
+
+    if(this.stomach.length < 10) {
+
+      this.stomach.push(someFood);
+
+    }
+  }
+
+  poop() {
+
+    if(this.stomach.length > 0) {
+
+      this.stomach = [];
+
+    }
+  }
+
+  toString() {
+
+    return this.name + ", " + this.age;
+
+  }
 
 }
 
